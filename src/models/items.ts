cefd31b0221627aba477d1c3f2b322222946bf2a
Zakir Hussain <itsjakirhussain@gmail.com>
2024-04-28 01:37:48 +0530

    Add items

--- src/models/items.ts
+++ src/models/items.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod';
 
-import { PRODUCT_TYPE } from './common';
+import { IdObjectSchema, PRODUCT_TYPE } from './common';
 import { number, string } from './helpers';
 
 export type ItemCreateSchemaType = z.infer<typeof ItemCreateSchema>;
@@ -14,4 +14,32 @@ export const ItemCreateSchema = z.object({
   quantity: number({ min: 0, max: 10000 }),
   position: number({ min: 0, max: 10000, optional: true }),
   productId: string(),
+  status: z.enum(['DRAFT', 'SCHEDULED', 'PLACED', 'ACCEPTED', 'PROGRESS', 'PREPARED']).optional(),
+});
+
+export type ItemUpdateSchemaType = z.infer<typeof ItemUpdateSchema>;
+
+export const ItemUpdateSchema = z.object({
+  title: z.string().optional(),
+  note: z.string().optional(),
+  type: z.enum(['NON_VEG', 'VEG']).optional(), // Assuming PRODUCT_TYPE is an enum with these values
+  price: z.number().optional(),
+  quantity: z.number().optional(),
+  total: z.number().optional(),
+  position: z.number().optional(),
+  placeAt: z.date().optional(),
+  placedAt: z.date().optional(),
+  acceptedAt: z.date().optional(),
+  prepared: z.date().optional(),
+  status: z.enum(['DRAFT', 'SCHEDULED', 'PLACED', 'ACCEPTED', 'PROGRESS', 'PREPARED']).optional(),
+  productId: z.string().optional(),
+  orderId: z.string().optional(),
+  billId: z.string().optional(),
+});
+
+export type ItemUpdateReqSchemaType = z.infer<typeof ItemUpdateReqSchema>;
+
+export const ItemUpdateReqSchema = z.object({
+  body: ItemUpdateSchema,
+  params: IdObjectSchema,
 });
